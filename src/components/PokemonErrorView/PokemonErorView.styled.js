b61112e8668ec7f058f576a7aa450f1f16f35475
Alex <afanasbev1@gmail.com>
2023-05-29 21:56:12 +0300

    upd

---
+++ src/components/PokemonErrorView/PokemonErorView.styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const PokemonWrong = styled.div`
+display: block;
+text-align: center;
+margin-left: 0 auto;
+margin-right: 0 auto;
+`
--- src/components/PokemonErrorView/PokemonErrorView.jsx
+++ src/components/PokemonErrorView/PokemonErrorView.jsx
@@ -1,10 +1,10 @@
 import imgEror from 'components/img/sticker.webp';
-
+import { PokemonWrong } from './PokemonErorView.styled';
 export default function PokemonErrorView({ message }) {
   return (
-    <div role="alert">
+    <PokemonWrong role="alert">
       <img src={imgEror} width="240" alt="sadcat" />
       <p>{message}</p>
-    </div>
+    </PokemonWrong>
   );
 }
